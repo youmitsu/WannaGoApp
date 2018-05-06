189f86e079d10e6647e91ef92fbf80c566731cb1
三堀　裕 <ymbullseye@gmail.com>
2018-05-06 18:23:56 +0900

    customAttribute適用

--- app/src/main/java/youmeee/co/jp/wannagoapp/presentation/customview/InputItemView.kt
+++ app/src/main/java/youmeee/co/jp/wannagoapp/presentation/customview/InputItemView.kt
@@ -4,20 +4,33 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.input_item_view.view.*
 import youmeee.co.jp.wannagoapp.R
 
 /**
  * Created by yumitsuhori on 2018/04/30.
  */
 class InputItemView : LinearLayout {
+
+    var itemName: String? = null
+
     @JvmOverloads
     constructor(
             context: Context,
             attrs: AttributeSet? = null,
             defStyle: Int = 0
-    ) : super(context, attrs, defStyle)
-
-    init {
+    ) : super(context, attrs, defStyle) {
         LayoutInflater.from(context).inflate(R.layout.input_item_view, this)
+        val a = context.theme.obtainStyledAttributes(attrs, R.styleable.inputItemView, 0, 0)
+        try {
+            itemName = a.getString(R.styleable.inputItemView_itemName)
+        } finally {
+            a.recycle()
+        }
+        buildView()
+    }
+
+    private fun buildView() {
+        item_title.text = itemName
     }
 }
