23bec80dc3f73950ebc1f119197666e43bf12c3b
youmitsu <ymbullseye@gmail.com>
2018-07-02 01:41:02 +0900

    Added date form

--- app/src/main/java/youmeee/co/jp/wannagoapp/presentation/activity/InputActivity.kt
+++ app/src/main/java/youmeee/co/jp/wannagoapp/presentation/activity/InputActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_input.*
 import youmeee.co.jp.wannagoapp.R
 
 /**
@@ -15,11 +16,14 @@ class InputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input)
+
+        expected_date_area.setOnClickListener {
+            Toast.makeText(this, "Tapped", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.input_menu, menu)
-
         return true
     }
 
